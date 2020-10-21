ce98c9811c72a36f658ba735e156b76236a13aae
Kasimashi <Kasimashi@users.noreply.github.com>
2020-10-21 15:25:40 +0200

    Ajout Projet HelloWorld Nios II

---
+++ FPGA/Altera/DE10/DE10_Project/nios2/nios2_inst.v
@@ -0,0 +1,8 @@
+	nios2 u0 (
+		.clk_clk                             (<connected-to-clk_clk>),                             //                          clk.clk
+		.leds_external_connection_export     (<connected-to-leds_external_connection_export>),     //     leds_external_connection.export
+		.switches_external_connection_export (<connected-to-switches_external_connection_export>), // switches_external_connection.export
+		.button_external_connection_export   (<connected-to-button_external_connection_export>),   //   button_external_connection.export
+		.hex_external_connection_export      (<connected-to-hex_external_connection_export>)       //      hex_external_connection.export
+	);
+
