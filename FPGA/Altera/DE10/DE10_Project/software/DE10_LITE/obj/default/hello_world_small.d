e2c302300654c0f102185ef346751c23f22611fc
Kasimashi <Kasimashi@users.noreply.github.com>
2020-12-07 23:24:07 +0100

    Mise à jour FPGA projet DE10-DE10-LITE_BASE & DE10-LITE_NIOSII

--- FPGA/Altera/DE10/DE10_Project/software/DE10_LITE/obj/default/hello_world_small.d
+++
@@ -1,4 +0,0 @@
-obj/default/hello_world_small.o: hello_world_small.c \
- C:/Users/Kasim/Desktop/Systemes-embarques/FPGA/Altera/DE10/DE10_Project/software/DE10_LITE_bsp/HAL/inc/sys/alt_stdio.h
-
-C:/Users/Kasim/Desktop/Systemes-embarques/FPGA/Altera/DE10/DE10_Project/software/DE10_LITE_bsp/HAL/inc/sys/alt_stdio.h:
